e0cd43676d5814a32db94f6f06adcbfd59d128fb
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-11-17 14:59:30 +0000

    program will now rerun if the user wants it to

--- backwards_num.py
+++ backwards_num.py
@@ -1,41 +1,59 @@
 #!/usr/bin/env python3
 # Copyright (c) 2022 Ioana Marinescu All rights reserved.
 # Created By: Ioana Marinescu
-#
+
 # Date: Nov. 7, 2022
 # a program reverses the order of a number
 
 
+def play_again():
+    while True:
+        # getting user input
+        replay = input("Would you like like run this program again? [y/n] ")
+
+        # if user want to rerun the program
+        if replay == "y":
+            return True
+
+        # if user doesn't want to rerun the program
+        elif replay == "n":
+            return False
+
+        # user didn't have a valid input
+        else:
+            print("Input Invalid! Please enter either y or n!")
+
 def main():
     # variables
-    user_num = input("Please enter any number: ")
-    is_not_valid = True
-    num_reversed = ""
     each_digit = []
 
+    while True:
+        # get user input
+        user_num = input("Please enter any number: ")
+
     # exception handling
-    while is_not_valid:
-        # tries turning user input from sting to int
         try:
             int(user_num)
 
         # if user num is not an int
         except Exception:
             print(f"{user_num} is not an integer!")
-            user_num = input("Please enter any number: ")
 
         # user num is an int
         else:
-            # breaks the loop
-            is_not_valid = False
 
             # putting each digit in a list
             each_digit = list(user_num)
 
             # reverses the order of the numbers
-            for counter_two in range(len(each_digit) - 1, -1, -1):
-                print(each_digit[counter_two], end="")
+            for counter in range(len(each_digit) - 1, -1, -1):
+                print(each_digit[counter], end="")
             print()
+
+        if play_again():
+            continue
+        else:
+            break
 
 
 if __name__ == "__main__":
